b849a43f1fddf87ba8b2bd9d507074d4ed93166b
lemeryfertitta <lemeryfertitta@Lukes-MacBook-Pro-2.local>
2014-10-31 23:33:01 -0400

    GUI + Main

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
+#!/usr/bin/env python
 try:
    import cPickle as pickle
 except:
    import pickle
-
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
 
+
+import os
+
+# class Menu(BoxLayout):
+# class MenuDialog(BoxLayout):
+#     pass
 
 class MainTextInput(TextInput):
     def __init__(self):
         self.start_convert = 0
         self.math_mode = 0
         self.data = []
-
         self.saved_file = ''
-
         TextInput.__init__(self)
 
     def insert_text(self, substring, from_undo=False):
         # s = substring.upper()
+
         if (substring == "$"):
             self.math_mode = 1-self.math_mode 
             if self.math_mode: # starting an equation
@@ -66,42 +74,61 @@ class MainTextInput(TextInput):
             # invalid file_name
             return None
 
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
-class SimpleEditorApp(App):
-    def build(self):
-        root = FloatLayout(orientation='horizontal', size_hint=(1, 1))
-        textinput = MainTextInput()
-        textinput.focus = True
-        # textinput.bind(text=on_text)
-        root.add_widget(textinput)
-        return root
 
-      
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class Root(FloatLayout):
+    loadfile = ObjectProperty(None)
+    savefile = ObjectProperty(None)
+    #editor = ObjectProperty(None)
+    #menu = ObjectProperty(None)
+    #infobar = ObjectProperty(None)
+    text_input = MainTextInput()
+    # menu = ObjectProperty(None)
+
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def show_save(self):
+        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Save file", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        with open(os.path.join(path, filename[0])) as stream:
+            self.text_input.text = stream.read()
+
+        self.dismiss_popup()
+
+    def save(self, path, filename):
+        self.text_input.save_data(os.path.join(path, filename))
+        # with open(os.path.join(path, filename), 'w') as stream:
+        #     stream.write(self.text_input.text)
+
+        self.dismiss_popup()
+
+
+class Editor(App):
+    pass
+
+
+# Factory.register('MenuDialog', cls=MenuDialog)
+#Factory.register('MenuDialog', cls=MenuDialog)
+
+
 if __name__ == '__main__':
-    SimpleEditorApp().run()
-
-
-# math/code/science mode (i.e. latex interpretation but with natural language (wolfram?))
-# free draw mode (possibly with drawing recoginition? i.e. draw-to-text)
-# graph mode, charts, diagrams, webs, flow charts etc etc etc 
-# timeline creation from input dates
-# map mode (tex to map)
-# create a link between places in your notes
-# auto-completion
-# vocab storage
-# headings
-# marking as important
-# term - def - example structure
-# collapsable everything (images and defs and such)
-# lists, bulletting
-# subject based themes
-# quick pull relevant images off the internet
-# orgranization
-# pre-dated; already have course info
-# quick wizard when creating notes for a new class
-# pull examples from other peoples notes? (Sharing based)
-# word command followed by common symbol or key sequence	
-# heading hierarchy HEADING - SUBHEADING etc...
-# ability to click on words and get definition from previous areas in notes
-# highlighting words with options
-# be able to quick pull more information about what you're taking notes on in general
+    Editor().run()
