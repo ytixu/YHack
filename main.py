8e96883a772d4b9a05be7055ccfb9c0aeb339406
coralam17@gmail.com <coralam17@gmail.com>
2014-10-31 22:19:31 -0400

    GUI Commit

--- main.py
+++ main.py
@@ -1,49 +1,72 @@
+#!/usr/bin/env python
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
-from kivy.uix.textinput import TextInput
-
-class MainTextInput(TextInput):
-
-    def insert_text(self, substring, from_undo=False):
-        s = substring.upper()
-        return super(MainTextInput, self).insert_text(s, from_undo=from_undo)
+from kivy.uix.boxlayout import BoxLayout
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
 
 
-class SimpleEditorApp(App):
-    def build(self):
-        root = FloatLayout(orientation='horizontal', size_hint=(1, 1))
-        textinput = MainTextInput()
-        textinput.focus = True
-        # textinput.bind(text=on_text)
-        root.add_widget(textinput)
-        return root
-      
+import os
+
+class MenuDialog(BoxLayout):
+    pass
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class Root(FloatLayout):
+    loadfile = ObjectProperty(None)
+    savefile = ObjectProperty(None)
+    #editor = ObjectProperty(None)
+    menu = ObjectProperty(None)
+    #infobar = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def show_save(self):
+        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Save file", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        with open(os.path.join(path, filename[0])) as stream:
+            self.text_input.text = stream.read()
+
+        self.dismiss_popup()
+
+    def save(self, path, filename):
+        with open(os.path.join(path, filename), 'w') as stream:
+            stream.write(self.text_input.text)
+
+        self.dismiss_popup()
+
+
+class Editor(App):
+    pass
+
+
+Factory.register('Root', cls=Root)
+Factory.register('LoadDialog', cls=LoadDialog)
+Factory.register('SaveDialog', cls=SaveDialog)
+Factory.register('MenuDialog', cls=MenuDialog)
+
+
 if __name__ == '__main__':
-    SimpleEditorApp().run()
-
-
-# math/code/science mode (i.e. latex interpretation but with natural language (wolfram?))
-# free draw mode (possibly with drawing recoginition? i.e. draw-to-text)
-# graph mode, charts, diagrams, webs, flow charts etc etc etc 
-# timeline creation from input dates
-# map mode (tex to map)
-# create a link between places in your notes
-# auto-completion
-# vocab storage
-# headings
-# marking as important
-# term - def - example structure
-# collapsable everything (images and defs and such)
-# lists, bulletting
-# subject based themes
-# quick pull relevant images off the internet
-# orgranization
-# pre-dated; already have course info
-# quick wizard when creating notes for a new class
-# pull examples from other peoples notes? (Sharing based)
-# word command followed by common symbol or key sequence	
-# heading hierarchy HEADING - SUBHEADING etc...
-# ability to click on words and get definition from previous areas in notes
-# highlighting words with options
-# be able to quick pull more information about what you're taking notes on in general
+    Editor().run()
