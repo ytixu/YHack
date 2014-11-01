d02974926943ac8d20f08fed682af1bb095e8978
yitian <yi.t.xu@mail.mcgill.ca>
2014-11-01 15:57:30 -0400

    split input text box

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 try:
    import cPickle as pickle
 except:
@@ -5,10 +6,20 @@ except:
 
 from pyh import *
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
-
+from kivy.uix.popup import Popup
+from kivy.gesture import Gesture, GestureDatabase
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.graphics import Color
+from kivy.clock import Clock
 
 class MainTextInput(TextInput):
     def __init__(self):
@@ -118,43 +129,86 @@ class MainTextInput(TextInput):
     #     page.printOut()
 
 
+class TextInputer(TextInput):
 
+    def __init__(self):
+        TextInput.__init__(self)
+        self.ctrl = False
+
+    def set_textdisplay(self,textdisplay):
+        self.textdisplay = textdisplay
+
+    def insert_text(self, substring, from_undo=False):
+        self.textdisplay.cursor = self.cursor
+        Clock.schedule_once(lambda dt: self.display_t(self.textdisplay.insert_text, substring, from_undo))
+        return super(TextInputer, self).insert_text(substring)
+
+    def do_backspace(self, from_undo=False, mode='bkspc'):
+        self.textdisplay.cursor = self.cursor
+        Clock.schedule_once(lambda dt: self.display_t(self.textdisplay.do_backspace, from_undo, mode))
+        return super(TextInputer, self).do_backspace(from_undo, mode)
+
+    def _keyboard_on_key_down(self, window, keycode, text, modifiers):
+        # print keycode, modifiers
+        _, keyname = keycode 
+        if keyname == 'ctrl':
+            self.ctrl = True
+        if self.ctrl and keyname == 'spacebar':
+            self.ctrl = False
+            self.selectMath()
+        super(TextInputer, self)._keyboard_on_key_down(window, keycode, text, modifiers)
+
+    def _keyboard_on_key_up(self, window, keycode):
+        self.ctrl = False
+        super(TextInputer, self)._keyboard_on_key_up(window, keycode)
+
+    def delete_selection(self, from_undo=False):
+        self.textdisplay.selection_text = self.selection_text
+        self.textdisplay._selection = self._selection
+        self.textdisplay._selection_from, self.textdisplay._selection_to = self._selection_from, self._selection_to
+        self.display_t(self.textdisplay.delete_selection, from_undo)
+        super(TextInputer, self).delete_selection(from_undo)
+
+    def selectMath(self): # almost the same as double tap
+        ci = self.cursor_index()
+        cc = self.cursor_col
+        line = self._lines[self.cursor_row]
+        len_line = len(line)
+        start = max(0, len(line[:cc]) - line[:cc].rfind(u' ') - 1)
+        end = line[cc:].find(u' ')
+        end = end if end > - 1 else (len_line - cc)
+        self.select_text(ci - start, ci + end)
+        print self.selection_text 
+        # TODO: to image and blah...
+
+    def display_t(self, func, *args):
+        # print self.textdisplay.selection_text
+        self.textdisplay.readonly = False
+        func(*args)  
+        self.textdisplay.readonly = True
+
+
+class TextEditor(GridLayout):
+    
+    def __init__(self):
+        GridLayout.__init__(self, cols=1, rows=2)
+        textdisplay = TextInput()
+        textdisplay.readonly = True
+        self.textinput = TextInputer()
+        self.textinput.set_textdisplay(textdisplay)
+        self.textinput.focus = True
+        self.add_widget(textdisplay)
+        self.add_widget(self.textinput)
 
 class SimpleEditorApp(App):
     def build(self):
-        root = FloatLayout(orientation='horizontal', size_hint=(1, 1))
-        textinput = MainTextInput()
-        textinput.focus = True
+        self.root = TextEditor()
+        # editor = TextEditor()
+        # editor.focus = True
         # textinput.bind(text=on_text)
-        root.add_widget(textinput)
-        return root
+        # root.add_widget(editor)
+        return self.root
 
       
 if __name__ == '__main__':
     SimpleEditorApp().run()
-
-
-# math/code/science mode (i.e. latex interpretation but with natural language (wolfram?))
-# free draw mode (possibly with drawing recoginition? i.e. draw-to-text)
-# graph mode, charts, diagrams, webs, flow charts etc etc etc 
-# timeline creation from input dates
-# map mode (tex to map)
-# create a link between places in your notes
-# auto-completion
-# vocab storage
-# headings
-# marking as important
-# term - def - example structure
-# collapsable everything (images and defs and such)
-# lists, bulletting
-# subject based themes
-# quick pull relevant images off the internet
-# orgranization
-# pre-dated; already have course info
-# quick wizard when creating notes for a new class
-# pull examples from other peoples notes? (Sharing based)
-# word command followed by common symbol or key sequence	
-# heading hierarchy HEADING - SUBHEADING etc...
-# ability to click on words and get definition from previous areas in notes
-# highlighting words with options
-# be able to quick pull more information about what you're taking notes on in general
