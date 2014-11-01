609dff859630d57c2c93b9152968aca1d795bbfc
yitian <yi.t.xu@mail.mcgill.ca>
2014-11-01 11:57:56 -0400

    Temp

--- main.py
+++ main.py
@@ -12,31 +12,43 @@ from kivy.uix.textinput import TextInput
 
 class MainTextInput(TextInput):
     def __init__(self):
+        TextInput.__init__(self)
+        
         self.start_convert = 0
         self.math_mode = 0
         self.data = []
+        self.image_dir = {}
 
         self.saved_file = ''
         self.HTML_file = ''
 
-        TextInput.__init__(self)
+        self.original_textbox = TextInput()
+
 
     def insert_text(self, substring, from_undo=False):
-        # s = substring.upper()
         if (substring == "$"):
             self.math_mode = 1-self.math_mode 
-            if self.math_mode: # starting an equation
-                text = self._get_text()[self.start_convert:]
-                self.data.append(text)
-            else: # ending an equation
+            if not self.math_mode: # ending an equation
                 text = self._get_text()[self.start_convert:]
                 ### TODO: search on wolfram 
                 ### possibly save picture in disk, and just use the file directory
                 result_image = None 
-                self.data.append((text, result_image))
+                self.image_dir[text] = result_image 
             self.start_convert = self.cursor_index() + 1
-        print self.data
+        self.original_textbox.insert_text(substring, from_undo=from_undo)
         return super(MainTextInput, self).insert_text(substring, from_undo=from_undo)
+
+    def convert_to_data(self):
+        texts = self._get_text().split("$")
+        cursor_index = 0
+        for i, text in enumerate(texts):
+            cursor_index += 1
+            if not text:
+                continue
+            if i%2 == 0: # equations
+                 self.data.append(())
+            
+        print self.data
 
     # serialization
     def save_data(self, file_name = None):
@@ -68,42 +80,42 @@ class MainTextInput(TextInput):
             # invalid file_name
             return None
 
-    def to_HTML(self, file_name = None):
-        # assume no break within math mode
-        # assume break before and after math mode indicate equation centered
-        self.save_data()
+    # def to_HTML(self, file_name = None):
+    #     # assume no break within math mode
+    #     # assume break before and after math mode indicate equation centered
+    #     self.save_data()
 
-        if not file_name:
-            file_name = self.HTML_file
-            if not file_name:
-                # no file directory
-                return False
-        else:
-            self.HTML_file = file_name
+    #     if not file_name:
+    #         file_name = self.HTML_file
+    #         if not file_name:
+    #             # no file directory
+    #             return False
+    #     else:
+    #         self.HTML_file = file_name
 
-        page = PyH('title to be modified')
-        # page.addCSS('...', ...)
+    #     page = PyH('title to be modified')
+    #     # page.addCSS('...', ...)
 
-        current_para = ""
-        for d in self.data:
-            if type(d) = str:
-                paragraphs = d.split('\n')
-                paragraphs[0] = current_para + " " + paragraphs[0]
-                if d[-1] == '\n'
-                    current_para = ''
-                else:
-                    current_para = paragraphs[-1]
-                    paragraphs = paragraphs[:-1]
-                for para in paragraphs:
-                    page << p(para)
-            else: # tuple
-                if current_para:
-                    current_para += '<img src=%s, class="inline_img")>' % d[1] 
-                                # supposingly in a css file, we have descriptions for inline_img
-                else:
-                    page << center() << img(src=d[1])
+    #     current_para = ""
+    #     for d in self.data:
+    #         if type(d) == str:
+    #             paragraphs = d.split('\n')
+    #             paragraphs[0] = current_para + " " + paragraphs[0]
+    #             if d[-1] == '\n'
+    #                 current_para = ''
+    #             else:
+    #                 current_para = paragraphs[-1]
+    #                 paragraphs = paragraphs[:-1]
+    #             for para in paragraphs:
+    #                 page << p(para)
+    #         else: # tuple
+    #             if current_para:
+    #                 current_para += '<img src=%s, class="inline_img")>' % d[1] 
+    #                             # supposingly in a css file, we have descriptions for inline_img
+    #             else:
+    #                 page << center() << img(src=d[1])
 
-        page.printOut()
+    #     page.printOut()
 
 
 
